afa3cb99bd362b73026ba6f42206756d8084a386
Tal Cohen <cohental63@gmail.com>
2024-05-13 12:03:49 +0300

    Add recent activity component

---
+++ src/app/components/containers/RecentActivity.tsx
@@ -0,0 +1,9 @@
+export default function RecentActivity() {
+  return (
+    <div className="mr-auto p-8">
+      <h3 className="font-anton font-bold text-xl text-coffee">
+        Recent Activity
+      </h3>
+    </div>
+  );
+}
