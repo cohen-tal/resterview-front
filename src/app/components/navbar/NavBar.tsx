c9f20b79d6868dba9347405f6ee1c460f70dae51
Tal Cohen <cohental63@gmail.com>
2024-06-03 10:08:00 +0300

    add navbar component

--- src/app/components/navbar/NavBar.tsx
+++ src/app/components/navbar/NavBar.tsx
@@ -5,18 +5,18 @@ import Image from "next/image";
 import SearchBox from "./SearchBox";
 
 interface NavBarProps {
-  layout: "mobile" | "desktop";
+  layout?: "mobile" | "desktop";
   children?: ReactNode;
 }
 
 export default function NavBar({ layout, children }: NavBarProps) {
   return (
-    <div className="flex flex-col items-center bg-jasper dark:bg-jasper-300">
-      <div className="flex items-center justify-between w-full pl-4 pr-4 z-50 text-pale_dogwood ">
-        <Image src="/logo.png" alt="logo" width={64} height={64} />
-        <BurgerMenu>test test</BurgerMenu>
+    <div className="flex flex-col items-center bg-transparent">
+      <div className="flex items-center justify-between w-full border p-5 z-50 mb-6">
+        {/* <Image src="/logo.png" alt="logo" width={64} height={64} /> */}
+        {/* <BurgerMenu>test test</BurgerMenu> */}
+        <SearchBox />
       </div>
-      {/* <SearchBox /> */}
     </div>
   );
 }
