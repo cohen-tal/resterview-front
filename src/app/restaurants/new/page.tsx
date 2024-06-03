064b8ff712f7033d70058fceb7a67891f6241929
Tal Cohen <cohental63@gmail.com>
2024-06-03 10:09:59 +0300

    add new restaurant form page

---
+++ src/app/restaurants/new/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import FormInput from "@/app/components/form/FormInput";
+import FormStepper from "@/app/components/form/FormStepper";
+import NewRestaurantForm from "@/app/components/form/NewRestaurantForm";
+import { useState } from "react";
+
+export default function NewRestaurantPage() {
+  return <NewRestaurantForm />;
+}
