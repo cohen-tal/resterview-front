eca0cdb12f7d25e39fb12e2b435b4ba55c0b9d9c
Tal Cohen <cohental63@gmail.com>
2024-06-06 14:28:05 +0300

    refactor map component

--- src/app/components/map/Map.tsx
+++ src/app/components/map/Map.tsx
@@ -1,18 +1,23 @@
 "use client";
 import { MapContainer } from "react-leaflet/MapContainer";
 import { TileLayer } from "react-leaflet/TileLayer";
-import { useMap } from "react-leaflet/hooks";
-import { Marker, Popup } from "react-leaflet";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
-import { Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
+import { ReactNode, useState } from "react";
 
-export default function Map() {
-  const myIcon: Icon = new Icon({
-    iconUrl: "https://img.icons8.com/ultraviolet/40/marker.png",
-    iconSize: [40, 40],
-  });
+interface MapProps {
+  markerTitle?: string;
+  markerContent?: string;
+  children?: ReactNode;
+  onMarkerDrag?: (lat: number, lng: number) => void;
+}
 
+export default function Map({
+  markerTitle = "",
+  markerContent,
+  onMarkerDrag,
+  children,
+}: MapProps) {
+  const [markerPos, setMarkerPos] = useState<[number, number] | undefined>();
   return (
     <div>
       <MapContainer
@@ -30,9 +35,7 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker icon={myIcon} position={[32.109333, 34.855499]}>
-          <Popup>🐻🍻🎉</Popup>
-        </Marker>
+        {children}
       </MapContainer>
     </div>
   );
