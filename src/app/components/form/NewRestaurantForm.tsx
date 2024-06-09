cf54ffddecb57b87ddd6dc3c18210019a5f6567e
Tal Cohen <cohental63@gmail.com>
2024-06-09 16:04:23 +0300

    add snackbar on form submit success / failure

--- src/app/components/form/NewRestaurantForm.tsx
+++ src/app/components/form/NewRestaurantForm.tsx
@@ -1,22 +1,21 @@
 "use client";
-import { ReactNode, useCallback, useState } from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useCallback, useState } from "react";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import { Variants, motion } from "framer-motion";
 import z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-
-import FormInput from "./FormInput";
 import FormStepper from "./FormStepper";
 import Map from "../map/Map";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import InputControllerWrapper from "../wrappers/InputControllerWrapper";
 import NewRestaurantButton from "../buttons/NewRestaurantButton";
 import InputSearchBox from "./InputSearchBox";
 import LocationMarker from "../map/LocationMarker";
 import { RestaurantFormSchema } from "@/utils/validation";
-import CircularProgress from "../progress/CircularProgress";
 import SubmitFormButton from "./SubmitFormButton";
+import ImageUpload from "../image-upload/ImageUpload";
+import Snackbar from "@mui/material/Snackbar";
+import SnackbarContent from "@mui/material/SnackbarContent";
+import { Alert } from "@mui/material";
 
 const variants: Variants = {
   left: {
@@ -33,52 +32,14 @@ const variants: Variants = {
   },
 };
 
-function renderNextButton(
-  onClick: () => void,
-  disabled: boolean = true,
-  type: "button" | "reset" | "submit" = "button"
-): ReactNode {
-  return (
-    <motion.button
-      className="absolute flex items-center justify-center border border-slate-300 rounded-full w-28 h-12 bg-slate-100/30 right-12 mt-1 mr-6 shadow-sm"
-      type={type}
-      title="Next"
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.9 }}
-      transition={{ type: "spring", stiffness: 400, damping: 10 }}
-      onClick={onClick}
-      disabled={disabled}
-    >
-      Next
-      <ArrowForwardIcon />
-    </motion.button>
-  );
-}
-
-function renderPrevButton(onClick: () => void): ReactNode {
-  return (
-    <motion.button
-      className="absolute flex items-center justify-center border border-slate-300 rounded-full w-28 h-12 bg-slate-100/30 left-12 mt-1 ml-6 shadow-sm"
-      type="button"
-      title="Next"
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.9 }}
-      transition={{ type: "spring", stiffness: 400, damping: 10 }}
-      onClick={onClick}
-    >
-      <ArrowBackIcon />
-      Previous
-    </motion.button>
-  );
-}
-
 export default function NewRestaurantForm() {
   const {
     register,
     watch,
+    clearErrors,
     handleSubmit,
     setValue,
-    formState: { errors, isValid, isSubmitting, isSubmitSuccessful },
+    formState: { errors, isSubmitting, isSubmitSuccessful },
     control,
   } = useForm<z.infer<typeof RestaurantFormSchema>>({
     resolver: zodResolver(RestaurantFormSchema),
@@ -92,10 +53,28 @@ export default function NewRestaurantForm() {
     },
   });
 
-  const onSubmit: SubmitHandler<z.infer<typeof RestaurantFormSchema>> = (
+  const onSubmit: SubmitHandler<z.infer<typeof RestaurantFormSchema>> = async (
     data
   ) => {
-    console.log(data);
+    try {
+      const res = await fetch("http://localhost:8080/api/v1/restaurants", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!res.ok) {
+        // Handle HTTP errors
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+
+      const jsonResponse = await res.json();
+      console.log(jsonResponse);
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
   };
 
   const [step, setStep] = useState<string>("step1");
@@ -145,122 +124,150 @@ export default function NewRestaurantForm() {
   }
 
   return (
-    <div className="relative flex flex-col items-center justify-center w-96 lg:w-[45%] h-[500px] border m-auto rounded-lg shadow-lg overflow-hidden">
-      <FormStepper stepToAnimate={step} />
-
-      <div className="relative w-full h-full overflow-hidden">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <motion.div
-            initial="center"
-            animate={direction[0]}
-            variants={variants}
-            className="absolute w-full"
-          >
-            <InputControllerWrapper
-              name="name"
-              type="text"
-              placeholder="Restaurant name*"
-              index={1}
-              control={control}
-            />
-            <InputControllerWrapper
-              name="description"
-              type="text"
-              placeholder="Description"
-              index={2}
-              control={control}
-              isTextArea
-            />
-            <NewRestaurantButton
-              type={"button"}
-              title="Next"
-              text="Next"
-              icon="next"
-              onClick={handleSubmitStep1}
-              disabled={
-                errors.name || errors.description || watch("name") === undefined
-                  ? true
-                  : false
-              }
-            />
-          </motion.div>
-          <motion.div
-            initial="right"
-            animate={direction[1]}
-            variants={variants}
-            className="absolute w-full"
-          >
-            <input type="text" hidden {...register("address")} />
-            <input type="text" hidden {...register("lat")} />
-            <input type="text" hidden {...register("lng")} />
-            <InputSearchBox
-              index={3}
-              valid={
-                watch("address") === undefined || watch("address") === ""
-                  ? undefined
-                  : errors.address || errors.lat || errors.lng
-                  ? false
-                  : true
-              }
-              onItemClick={[
-                setCoords,
-                (value: string) =>
-                  setValue("address", value, { shouldValidate: true }),
-              ]}
-            />
-            <div className="p-4">
-              <Map>
-                <LocationMarker
-                  title={watch("name")}
-                  content={watch("description")}
-                  draggable
-                  onDrag={setCoords}
-                  position={[watch("lat"), watch("lng")]}
-                />
-              </Map>
-            </div>
-            <NewRestaurantButton
-              type={"button"}
-              title="Next"
-              text="Next"
-              icon="next"
-              onClick={handleSubmitStep2}
-              disabled={
-                errors.address ||
-                errors.lat ||
-                errors.lng ||
-                watch("address") === ""
-                  ? true
-                  : false
-              }
-            />
-            <NewRestaurantButton
-              type={"button"}
-              title="Previous"
-              text="Previous"
-              icon="prev"
-              onClick={handlePrevStep2}
-              disabled={false}
-            />
-          </motion.div>
-          <motion.div
-            initial="right"
-            animate={direction[2]}
-            variants={variants}
-            className="absolute w-full"
-          >
-            <FormInput
-              type="file"
-              accept="image/*"
-              multiple
-              name="images"
-              placeHolder="Upload Images"
-              index={5}
-            />
-            <SubmitFormButton submitting={isSubmitting} />
-          </motion.div>
-        </form>
+    <>
+      <div className="relative flex flex-col items-center justify-center w-96 lg:w-[45%] h-[500px] border m-auto rounded-lg shadow-lg overflow-hidden">
+        <FormStepper stepToAnimate={step} />
+        <div className="relative w-full h-full overflow-hidden">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <motion.div
+              initial="center"
+              animate={direction[0]}
+              variants={variants}
+              className="absolute w-full"
+            >
+              <InputControllerWrapper
+                name="name"
+                type="text"
+                placeholder="Restaurant name*"
+                index={1}
+                control={control}
+              />
+              <InputControllerWrapper
+                name="description"
+                type="text"
+                placeholder="Description"
+                index={2}
+                control={control}
+                isTextArea
+              />
+              <NewRestaurantButton
+                type={"button"}
+                title="Next"
+                text="Next"
+                icon="next"
+                onClick={handleSubmitStep1}
+                disabled={
+                  errors.name || errors.description || watch("name") === ""
+                    ? true
+                    : false
+                }
+              />
+            </motion.div>
+            <motion.div
+              initial="right"
+              animate={direction[1]}
+              variants={variants}
+              className="absolute w-full"
+            >
+              <input type="text" hidden {...register("address")} />
+              <input type="text" hidden {...register("lat")} />
+              <input type="text" hidden {...register("lng")} />
+              <InputSearchBox
+                index={3}
+                valid={
+                  watch("address") === undefined || watch("address") === ""
+                    ? undefined
+                    : errors.address || errors.lat || errors.lng
+                    ? false
+                    : true
+                }
+                onItemClick={[
+                  setCoords,
+                  (value: string) =>
+                    setValue("address", value, { shouldValidate: true }),
+                ]}
+              />
+              <div className="p-4">
+                <Map>
+                  <LocationMarker
+                    title={watch("name")}
+                    content={watch("description")}
+                    draggable
+                    onDrag={setCoords}
+                    position={[watch("lat"), watch("lng")]}
+                  />
+                </Map>
+              </div>
+              <NewRestaurantButton
+                type={"button"}
+                title="Next"
+                text="Next"
+                icon="next"
+                onClick={handleSubmitStep2}
+                disabled={
+                  errors.address ||
+                  errors.lat ||
+                  errors.lng ||
+                  watch("address") === ""
+                    ? true
+                    : false
+                }
+              />
+              <NewRestaurantButton
+                type={"button"}
+                title="Previous"
+                text="Previous"
+                icon="prev"
+                onClick={handlePrevStep2}
+                disabled={false}
+              />
+            </motion.div>
+            <motion.div
+              className="absolute flex flex-col w-full h-full"
+              initial="right"
+              animate={direction[2]}
+              variants={variants}
+            >
+              {/* <ImageUpload /> */}
+              <Controller
+                name="images"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <ImageUpload
+                    value={field.value}
+                    onChange={field.onChange}
+                    error={fieldState.error?.message}
+                  />
+                )}
+              />
+              <SubmitFormButton submitting={isSubmitting} />
+            </motion.div>
+          </form>
+        </div>
       </div>
-    </div>
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        open={isSubmitSuccessful || errors.images ? true : false}
+        autoHideDuration={2000}
+        onClose={() => {
+          clearErrors("images");
+        }}
+      >
+        {isSubmitSuccessful ? (
+          <Alert severity="success" variant="filled" sx={{ width: "100%" }}>
+            Restaurant added successfully!
+          </Alert>
+        ) : (
+          <Alert
+            // onClose={handleClose}
+            severity="error"
+            variant="filled"
+            sx={{ width: "100%" }}
+          >
+            Image size must be at most 3 MB.
+          </Alert>
+        )}
+      </Snackbar>
+    </>
   );
 }
