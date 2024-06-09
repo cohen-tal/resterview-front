cf1aa0f6a47c71be7bab4a83979044395d600f0f
Tal Cohen <cohental63@gmail.com>
2024-06-09 16:03:56 +0300

    refactor code and styling with states

--- src/app/components/form/SubmitFormButton.tsx
+++ src/app/components/form/SubmitFormButton.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import SendIcon from "@mui/icons-material/Send";
 import CircularProgress from "../progress/CircularProgress";
 
@@ -10,12 +11,22 @@ export default function SubmitFormButton({
   valid = false,
   submitting,
 }: SubmitFormButtonProps) {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="flex m-auto items-center justify-center gap-2 p-2 border rounded-md bg-blue-400 text-white font-bold py-2 px-4 w-28 h-12 transition duration-300 ease-in-out hover:bg-white hover:text-blue-500 hover:border-blue-500 disabled:hover:border-slate-300  disabled:bg-transparent disabled:text-slate-300  shadow-sm mt-1">
+    <div
+      className="flex m-auto mb-4 items-center gap-2 p-2 border rounded-md bg-blue-400 text-white font-bold w-28 h-12 transition duration-300 ease-in-out hover:bg-white hover:text-blue-500 hover:border-blue-500 disabled:hover:border-slate-300 disabled:bg-transparent disabled:text-slate-300 shadow-sm"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <button type="submit" disabled={valid}>
         Submit
       </button>
-      {submitting ? <CircularProgress /> : <SendIcon />}
+      {submitting ? (
+        <CircularProgress color={isHovered ? "rgb(59 130 246)" : "#FFFFFF"} />
+      ) : (
+        <SendIcon />
+      )}
     </div>
   );
 }
