3e9a282f7ce0c4c3f0a592bcdba9da9ea43c42a8
Tal Cohen <cohental63@gmail.com>
2024-06-11 09:54:20 +0300

    update searchbox design

--- src/app/components/navbar/SearchBox.tsx
+++ src/app/components/navbar/SearchBox.tsx
@@ -12,15 +12,15 @@ export default function SearchBox({
   onSelect,
 }: SearchBoxProps) {
   return (
-    <div className="relative flex flex-col items-center min-w-48 w-96 lg:w-[40%] p-0.5">
-      <div className="flex items-center bg-white shadow-inner w-full rounded-full h-12 lg:shadow-lg">
+    <div className="relative flex flex-col border items-center min-w-48 w-96 lg:w-[40%] rounded-md">
+      <div className="flex items-center shadow-inner w-full rounded-md h-12 lg:shadow-lg">
         <input
-          className="text-gray-400 w-full p-2 h-full text-lg focus:outline-none rounded-l-full lg:rounded-none border-none"
+          className="text-gray-600 w-full p-2 h-full text-lg focus:outline-none rounded-md border-none"
           placeholder={placeholder}
           onChange={onChange}
           onSelect={onSelect}
         />
-        <button className="bg-[#20262f] p-3 rounded-r-full h-full flex items-center justify-center">
+        <button className="bg-[#20262f] p-3 rounded-r-md h-full flex items-center justify-center">
           <SearchIcon className="text-white" />
         </button>
       </div>
