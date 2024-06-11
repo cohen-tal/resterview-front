bcd9832f03226d3687779bb1d15a9de788909f5e
Tal Cohen <cohental63@gmail.com>
2024-06-11 09:57:10 +0300

    add restaurants page skeleton design

---
+++ src/app/restaurants/page.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import RestaurantCard from "../components/cards/RestaurantCard";
+
+export default function RestaurantsPage() {
+  return (
+    <div className="grid grid-cols-4 p-20 pt-14 gap-8">
+      {/* <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard /> */}
+    </div>
+  );
+}
