cab87715af122559a89fbe4ac9f7bbe5056e14aa
Tal Cohen <cohental63@gmail.com>
2024-06-21 14:40:19 +0300

    refactor code to server component

--- src/app/components/navbar/NavBar.tsx
+++ src/app/components/navbar/NavBar.tsx
@@ -1,15 +1,9 @@
-"use client";
-import { ReactNode } from "react";
 import BurgerMenu from "../menus/BurgerMenu";
 import Image from "next/image";
 import SearchBox from "./SearchBox";
 import DesktopMenu from "../menus/DesktopMenu";
 
-interface NavBarProps {
-  children?: ReactNode;
-}
-
-export default function NavBar({ children }: NavBarProps) {
+export default function NavBar() {
   return (
     <nav className="sticky top-0 left-0 right-0 w-full bg-white shadow-sm z-[9999]">
       <div className="flex items-center justify-center flex-1 gap-4 border-b h-20 p-1">
