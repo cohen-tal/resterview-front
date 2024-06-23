252e34b530e92849f5b3f3d2a4704fb810c07d67
Tal Cohen <cohental63@gmail.com>
2024-06-23 17:26:17 +0300

    fix styling alignment

--- src/app/components/buttons/SliderNextPrevButton.tsx
+++ src/app/components/buttons/SliderNextPrevButton.tsx
@@ -1,11 +1,15 @@
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 
+interface SliderNextPrevButtonProps {
+  direction: "next" | "prev";
+  onClick: () => void;
+}
+
 export default function SliderNextPrevButton({
   direction,
-}: {
-  direction: "next" | "prev";
-}) {
+  onClick,
+}: SliderNextPrevButtonProps) {
   const isPrev = direction === "prev";
   return (
     <div
@@ -16,11 +20,16 @@ export default function SliderNextPrevButton({
       <div
         className={`flex items-center justify-start h-full p-1 ${
           isPrev
-            ? "before:bg-white before:h-full before:absolute before:left-0 before:w-[70%]"
-            : "after:bg-white after:h-full after:absolute after:right-0 after:w-[70%]"
+            ? "before:bg-white before:h-full before:absolute before:left-0 before:w-[60%]"
+            : "after:bg-white after:h-full after:absolute after:right-0 after:w-[60%]"
         }`}
       >
-        <button className="rounded-full border p-2 backdrop-blur-sm shadow-md hover:scale-110 z-50">
+        <button
+          className="rounded-full border p-0.5 sm:p-1 backdrop-blur-sm shadow-md hover:scale-110 z-50"
+          onClick={() => {
+            onClick();
+          }}
+        >
           {isPrev ? <ArrowBackIosNewIcon /> : <ArrowForwardIosIcon />}
         </button>
       </div>
