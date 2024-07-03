20c6a55dfd10b03c2fcd37b8a55bdc62e4ea6d9c
Tal Cohen <cohental63@gmail.com>
2024-07-03 15:54:48 +0300

    refactor code

--- src/app/components/containers/CategoriesContainer.tsx
+++ src/app/components/containers/CategoriesContainer.tsx
@@ -1,12 +1,9 @@
-import CategoriesMenu from "../menus/CategoriesMenu";
+import RestaurantCategoriesMenu from "../menus/RestaurantCategoriesMenu";
 
 export default function CategoriesContainer() {
   return (
     <div className="flex flex-col items-center justify-center gap-5 pt-10 pb-10">
-      {/* <h2 className=" self-start pl-4 font-sans font-bold">
-        Explore categories
-      </h2> */}
-      <CategoriesMenu />
+      <RestaurantCategoriesMenu />
     </div>
   );
 }
