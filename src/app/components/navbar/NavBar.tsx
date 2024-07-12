cdd91e839511b36742db2e494593ca9e74a57519
Tal Cohen <cohental63@gmail.com>
2024-07-12 17:20:33 +0300

    refine styling

--- src/app/components/navbar/NavBar.tsx
+++ src/app/components/navbar/NavBar.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 export default function NavBar() {
   return (
     <nav className="sticky top-0 left-0 right-0 w-full bg-white shadow-md z-[9999]">
-      <div className="flex items-center justify-center flex-1 gap-4 border-b h-20 p-1">
+      <div className="flex items-center justify-center flex-1 gap-4 border-b h-16 p-1">
         <Link href={"/"} className="mr-6 mb-3">
           <Image
             className="ml-4"
