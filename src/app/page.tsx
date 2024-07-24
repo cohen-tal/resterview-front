421c52a388509443ef70cbb2b51e3c6c932b9eb5
Tal Cohen <cohental63@gmail.com>
2024-07-24 13:48:17 +0300

    add data fetch from api backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,25 @@
 import Image from "next/image";
 import RecentReviewsContainer from "./components/containers/RecentReviewsContainer";
 import RecentRestaurantsContainer from "./components/containers/RecentRestaurantsContainer";
+import { useEffect, useState } from "react";
+import fetchAPI from "@/utils/fetchUtil";
+import { RecentRestaurant, RecentReview } from "../../d";
 
 export default function Home() {
+  const [recentReviews, setRecentReviews] = useState<RecentReview[]>();
+  const [recentRestaurants, setRecentRestaurants] =
+    useState<RecentRestaurant[]>();
+
+  useEffect(() => {
+    fetchAPI("/home").then((val) => {
+      console.dir(val);
+      setRecentRestaurants(val.restaurants);
+      setRecentReviews(val.reviews);
+    });
+  }, []);
+
   return (
-    <main className="main flex flex-col items-center dark:bg-slate-900 overflow-x-hidden">
+    <main className="main dark:bg-slate-900 overflow-x-hidden overflow-y-auto max-h-screen">
       <div className="relative flex flex-col items-center justify-center w-full h-80 lg:h-[40rem]">
         <h1
           className="font-sedan-sc whitespace-pre-wrap md:whitespace-normal lg:-mt-56 text-floral_white p-8 font-bold text-3xl lg:text-6xl z-50"
@@ -22,14 +37,13 @@ export default function Home() {
         />
       </div>
 
-      <section className="bg-white/40 dark:bg-slate-800 shadow-md rounded-full mb-8">
-        {/* {!matches && <SearchBox />} */}
-      </section>
+      {/* <section className="bg-white/40 dark:bg-slate-800 shadow-md rounded-full mb-8"></section> */}
 
       <section className="w-full bg-white/15 dark:bg-slate-800 shadow-sm mb-4">
-        <RecentReviewsContainer />
+        <RecentReviewsContainer reviews={recentReviews} />
       </section>
-      <RecentRestaurantsContainer />
+      <RecentRestaurantsContainer recents={recentRestaurants ?? []} />
+      <div className="h-[300px]"></div>
     </main>
   );
 }
