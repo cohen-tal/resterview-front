49a95c0e5fdad872691966f1288ee914e8d8fe1b
Tal Cohen <cohental63@gmail.com>
2024-07-24 13:58:35 +0300

    add util function for abstraction of fetching from API backend server

---
+++ src/utils/fetchUtil.ts
@@ -0,0 +1,23 @@
+import { RestaurantAPI, RestaurantCard, ReviewAPI } from "../../d";
+
+export default async function fetchAPI<
+  T extends RestaurantAPI | ReviewAPI | RestaurantAPI[] | RestaurantCard[] = any
+>(endpoint: string, init?: RequestInit): Promise<T> {
+  const url = "http://localhost:8080/api/v1" + endpoint;
+  try {
+    const response = await fetch(url, init);
+    const responseJson = await response.json();
+
+    if (!response.ok) {
+      throw new Error(
+        responseJson.message ||
+          `Error fetching data: ${response.status}: ${response.statusText}`
+      );
+    }
+
+    return responseJson as T;
+  } catch (err) {
+    console.error(err);
+    return Promise.reject(err);
+  }
+}
