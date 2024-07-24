0da9dd1c7999176564dcbb1684fb044c49dcc35e
Tal Cohen <cohental63@gmail.com>
2024-07-24 13:47:50 +0300

    change config

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,11 @@ const nextConfig = {
         hostname: "avatars.githubusercontent.com",
         port: "",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+      },
     ],
   },
 };
