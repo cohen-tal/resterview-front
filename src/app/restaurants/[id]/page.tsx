442970e4a1e13f30fb62c6f96eb604d133cb54ea
Tal Cohen <cohental63@gmail.com>
2024-07-28 16:01:02 +0300

    refactor code to re-render page when user edit/delete his own review

--- src/app/restaurants/[id]/page.tsx
+++ src/app/restaurants/[id]/page.tsx
@@ -6,7 +6,7 @@ import ReviewCard from "@/app/components/reviews/ReviewCard";
 import SwiperSliderContainer from "@/app/components/containers/SwiperSliderContainer";
 import Image from "next/image";
 import { SwiperSlide } from "swiper/react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import AllReviewsContainer from "@/app/components/containers/AllReviewsContainer";
 import StarRatingInput from "@/app/components/rating/StarRatingInput";
 import { Restaurant, RestaurantAPI, ReviewAPI } from "../../../../d";
@@ -55,17 +55,21 @@ export default function RestaurantPage() {
   const { id } = useParams();
   const { data: session } = useSession();
 
-  useEffect(() => {
+  const handleFetching = useCallback(() => {
     const restaurantId: string = id as string;
     if (session?.accessToken) {
       fetchRestaurant(restaurantId, session.accessToken.token)
         .then((res) => setRestaurant(res))
         .catch((reason) => console.log(reason));
     }
-  }, [id, session]);
+  }, [id, session?.accessToken]);
+
+  useEffect(() => {
+    handleFetching();
+  }, [handleFetching]);
 
   if (!restaurant) {
-    return <div>Error loading restaurant</div>;
+    return <div>Loading restaurant</div>;
   }
 
   function renderImages() {
@@ -75,6 +79,10 @@ export default function RestaurantPage() {
       </SwiperSlide>
     ));
   }
+
+  const alreadyWroteReview: boolean = restaurant.reviews.some(
+    (review) => review.author.id === session?.id
+  );
 
   return (
     <>
@@ -117,17 +125,19 @@ export default function RestaurantPage() {
               <h2 className="text-2xl font-semibold flex items-center text-light-gray mb-3">
                 Recommended Reviews
               </h2>
-              <Link
-                href={`/reviews/writereview/${restaurant.id}`}
-                className="flex place-items-center p-4 pr-0 gap-3 border rounded-lg font-figtree shadow-md text-md text-blue-600"
-              >
-                Write a review
-                <StarRatingInput ratingType="stars" readOnly={false} />
-              </Link>
+              {!alreadyWroteReview && (
+                <Link
+                  href={`/reviews/writereview/${restaurant.id}`}
+                  className="flex place-items-center p-4 pr-0 gap-3 border rounded-lg font-figtree shadow-md text-md text-blue-600"
+                >
+                  Write a review
+                  <StarRatingInput ratingType="stars" readOnly={false} />
+                </Link>
+              )}
             </div>
             <div className="flex flex-col gap-16 lg:grid lg:grid-cols-2 lg:gap-14">
               {restaurant.reviews.map((review) => (
-                <ReviewCard key={review.id} review={review} />
+                <ReviewCard key={review.id + review.rating} review={review} />
               ))}
             </div>
             <button
@@ -151,9 +161,13 @@ export default function RestaurantPage() {
               ratingPercentages: restaurant.ratingPercentages,
             }}
             isOpen={isOpen}
+            alreadyWrittenReview={alreadyWroteReview}
             onClose={() => {
               setIsOpen(false);
               document.body.style.overflow = "auto";
+            }}
+            onReviewEdit={() => {
+              handleFetching();
             }}
           />
         )}
