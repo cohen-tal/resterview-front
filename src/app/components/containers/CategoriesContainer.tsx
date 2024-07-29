693722ea595accac84c04fc40f6d9bc1683b24ee
Tal Cohen <cohental63@gmail.com>
2024-07-29 13:59:15 +0300

    delete unused component

--- src/app/components/containers/CategoriesContainer.tsx
+++
@@ -1,9 +0,0 @@
-import RestaurantCategoriesMenu from "../menus/RestaurantCategoriesMenu";
-
-export default function CategoriesContainer() {
-  return (
-    <div className="flex flex-col items-center justify-center gap-5 pt-10 pb-10">
-      <RestaurantCategoriesMenu />
-    </div>
-  );
-}
