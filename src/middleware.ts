6aa534bf8beb1dcfaa88ea80743132c688b752ef
Tal Cohen <cohental63@gmail.com>
2024-08-06 11:05:22 +0300

    middleware test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
-import { auth } from "../auth";
+// import { auth } from "../auth";
 
-export default auth((req) => {
-  if (!req.auth || req.auth?.error) {
-    const newUrl = new URL("/login", req.nextUrl.origin);
-    return Response.redirect(newUrl);
-  }
-});
+// export default auth((req) => {
+//   if (!req.auth || req.auth?.error) {
+//     const newUrl = new URL("/login", req.nextUrl.origin);
+//     return Response.redirect(newUrl);
+//   }
+// });
 
-export const config = {
-  matcher: ["/restaurants/new", "/profile"],
-};
+// export const config = {
+//   matcher: ["/restaurants/new", "/profile"],
+// };
