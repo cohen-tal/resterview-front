12b2c248f533f20c823ff968ab30b283b66283e4
Tal Cohen <cohental63@gmail.com>
2024-08-07 15:04:59 +0300

    refactor code

--- src/app/components/profile/ProfileItem.tsx
+++ src/app/components/profile/ProfileItem.tsx
@@ -10,7 +10,7 @@ export default function ProfileItem({
   children,
 }: ProfileItemProps) {
   return (
-    <div className="profile-item flex flex-col flex-wrap content-center w-full">
+    <div className="profile-item flex flex-col w-fit">
       {title && <div className="pt-1 text-gray-400 font-figtree">{title}</div>}
       {content && (
         <div className=" text-light-gray font-medium font-figtree">
