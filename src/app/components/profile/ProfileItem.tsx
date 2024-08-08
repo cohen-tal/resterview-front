a6f7a819441850f08d75cca2ff8500108c70db2a
Tal Cohen <cohental63@gmail.com>
2024-08-08 17:28:33 +0300

    add classname prop for flexibility

--- src/app/components/profile/ProfileItem.tsx
+++ src/app/components/profile/ProfileItem.tsx
@@ -2,15 +2,17 @@ interface ProfileItemProps {
   title?: string;
   content?: string | null;
   children?: React.ReactNode;
+  className?: string;
 }
 
 export default function ProfileItem({
   content,
   title,
   children,
+  className,
 }: ProfileItemProps) {
   return (
-    <div className="profile-item flex flex-col w-fit">
+    <div className={"flex flex-col w-fit " + className}>
       {title && <div className="pt-1 text-gray-400 font-figtree">{title}</div>}
       {content && (
         <div className=" text-light-gray font-medium font-figtree">
