8f758c81c606096aebfd5ddfba99f0c854865ffb
Tal Cohen <cohental63@gmail.com>
2024-08-14 16:26:01 +0300

    refactor code and add images preview grid

--- src/app/components/cards/RecentReviewCard.tsx
+++ src/app/components/cards/RecentReviewCard.tsx
@@ -1,10 +1,9 @@
 "use client";
 import React from "react";
 import { RecentReview } from "../../../../d";
-import Paper from "@mui/material/Paper";
 import { Avatar } from "@mui/material";
-import { AnimationControls, motion } from "framer-motion";
 import StarRatingInput from "../rating/StarRatingInput";
+import ImagesGridContainer from "../containers/ImagesGridContainer";
 
 interface RecentReviewCardProps {
   review?: Partial<RecentReview>;
@@ -12,11 +11,8 @@ interface RecentReviewCardProps {
 
 export default function RecentReviewCard({ review }: RecentReviewCardProps) {
   return (
-    <div className="flex justify-center w-full max-h-48 hover:shadow-xl">
-      <Paper
-        className="min-w-fit max-w-xs bg-white border rounded-lg border-black/10 shadow-sm p-4 lg:hover:scale-110 transition duration-300"
-        elevation={0}
-      >
+    <div className="flex justify-center rounded-xl hover:shadow-xl">
+      <div className="w-[400px] border rounded-xl border-black/10 shadow-md p-4 transition duration-300">
         <div className="flex items-center gap-2">
           <Avatar src={review?.image} alt="user-avatar" />
           <div className="relative w-28 h-6">
@@ -28,8 +24,12 @@ export default function RecentReviewCard({ review }: RecentReviewCardProps) {
           </div>
         </div>
         <h3 className="text-light-gray font-figtree text-xl">{review?.name}</h3>
-        <div className="mt-2 max-w-72 line-clamp-3">{review?.text}</div>
-      </Paper>
+        <div className="mt-2 max-w-72 line-clamp-4">{review?.text}</div>
+        <ImagesGridContainer
+          userImage={review?.image ?? ""}
+          userName={review?.name!}
+        />
+      </div>
     </div>
   );
 }
