ab2b1f2ceaf81e1e4fcc467f9499b643abece07a
Tal Cohen <cohental63@gmail.com>
2024-08-14 16:20:53 +0300

    update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import RecentRestaurantsContainer from "./components/containers/RecentRestaurant
 import { useEffect, useState } from "react";
 import fetchAPI from "@/utils/fetchUtil";
 import { RecentRestaurant, RecentReview, Recents } from "../../d";
+import Footer from "./components/footer/Footer";
 
 export default function Home() {
   const [recentReviews, setRecentReviews] = useState<RecentReview[]>();
@@ -19,26 +20,32 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="main dark:bg-slate-900 overflow-hidden">
-      <div className="relative flex flex-col items-center justify-center w-full h-80 lg:h-[40rem]">
-        <h1
-          className="font-sedan-sc whitespace-pre-wrap md:whitespace-normal lg:-mt-56 text-floral_white p-8 font-bold text-3xl lg:text-6xl z-50"
-          style={{ textShadow: "0 0 2px #000, 0 0 3px #000" }}
-        >
-          {"Savor the Reviews.\nDiscover the Flavors."}
-        </h1>
-        <Image
-          src={"/landing1.jpg"}
-          alt="Landing page picture"
-          layout="fill"
-          style={{ objectFit: "cover" }}
-          quality={100}
-        />
-      </div>
-      <section className="w-full bg-white/15 dark:bg-slate-800 shadow-sm mb-4">
-        <RecentReviewsContainer reviews={recentReviews ?? []} />
-      </section>
-      <RecentRestaurantsContainer recents={recentRestaurants ?? []} />
-    </main>
+    <>
+      <main className="dark:bg-slate-900">
+        <div className="relative flex flex-col items-center justify-center w-full h-80 lg:h-[40rem]">
+          <h1
+            className="font-sedan-sc whitespace-pre-wrap md:whitespace-normal text-floral_white p-8 font-bold text-3xl lg:text-6xl z-50"
+            style={{ textShadow: "0 0 2px #000, 0 0 3px #000" }}
+          >
+            {"Savor the Reviews.\nDiscover the Flavors."}
+          </h1>
+          <div className="absolute inset-0 bg-black/10 z-10" />
+          <Image
+            src={"/landing-image.jpg"}
+            alt="Landing page picture"
+            layout="fill"
+            style={{ objectFit: "cover" }}
+            quality={100}
+          />
+        </div>
+        <section className="w-full bg-white/15 dark:bg-slate-800 shadow-sm mb-4">
+          <RecentReviewsContainer reviews={recentReviews ?? []} />
+        </section>
+        <section className="w-full bg-white/15 dark:bg-slate-800 mb-4">
+          <RecentRestaurantsContainer recents={recentRestaurants ?? []} />
+        </section>
+      </main>
+      <Footer />
+    </>
   );
 }
