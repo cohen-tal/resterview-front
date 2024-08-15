51f0dd1adf01580bc65c9f9264f25598fc360e57
Tal Cohen <cohental63@gmail.com>
2024-08-15 12:41:12 +0300

    refactor code and add log out button

--- src/app/components/menus/BurgerMenu.tsx
+++ src/app/components/menus/BurgerMenu.tsx
@@ -5,26 +5,32 @@ import CloseIcon from "@mui/icons-material/Close";
 import Image from "next/image";
 import BurgerMenuItem from "./menu-items/BurgerMenuItem";
 import BurgerMenuSection from "./menu-sections/BurgerMenuSection";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, useSession, signOut } from "next-auth/react";
 import BurgerMenuProfileButton from "../buttons/BurgerMenuProfileButton";
 import { GiForkKnifeSpoon } from "react-icons/gi";
 import {
   MdOutlineAddBusiness,
-  MdOutlineWatchLater,
   MdOutlineRateReview,
   MdOutlineReviews,
 } from "react-icons/md";
-import { BsBookmarkStar } from "react-icons/bs";
-import { VscOpenPreview } from "react-icons/vsc";
-import { IoTrophyOutline } from "react-icons/io5";
-import { LiaMapMarkedAltSolid } from "react-icons/lia";
+
 import { IconContext } from "react-icons";
 import { FcGoogle } from "react-icons/fc";
 import { ImGithub } from "react-icons/im";
-
+import { useEffect } from "react";
 export default function BurgerMenu() {
   const [isOpen, toggle] = useCycle(false, true);
   const { data: session } = useSession();
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    }
+    // Cleanup function to set overflow to auto when the component unmounts
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
 
   return (
     <div className="sm:hidden">
@@ -39,7 +45,7 @@ export default function BurgerMenu() {
       </div>
       <AnimatePresence>
         {isOpen && (
-          <div className="flex flex-col items-end backdrop-blur-sm fixed inset-0 z-40 bg-slate-800 bg-opacity-30">
+          <div className="fixed flex flex-col items-end backdrop-blur-sm inset-0 z-40 bg-slate-800 bg-opacity-30">
             <motion.div
               initial={{ opacity: 0, x: "100%" }}
               animate={{ opacity: 1, x: 0, transition: { duration: 0.3 } }}
@@ -67,6 +73,7 @@ export default function BurgerMenu() {
                 <BurgerMenuProfileButton
                   src={session.user.image!}
                   name={session.user.name!}
+                  onClick={toggle}
                 />
               ) : (
                 <IconContext.Provider
@@ -74,7 +81,7 @@ export default function BurgerMenu() {
                     size: "2.5rem",
                   }}
                 >
-                  <div className="w-[85%] m-auto flex flex-col items-center justify-center gap-4">
+                  <div className="w-[85%] mx-auto flex flex-col items-center justify-center gap-4">
                     <button
                       className="flex justify-center items-center p-1 gap-4 w-full border rounded-xl shadow-md"
                       onClick={() => {
@@ -118,13 +125,6 @@ export default function BurgerMenu() {
                     >
                       <MdOutlineAddBusiness />
                     </BurgerMenuItem>
-                    <BurgerMenuItem
-                      text="Favorite Restaurants"
-                      href="/restaurants/favorites"
-                      onClick={toggle}
-                    >
-                      <BsBookmarkStar />
-                    </BurgerMenuItem>
                   </BurgerMenuSection>
                 </div>
                 <div className="self-center border-b w-[90%]">
@@ -134,32 +134,24 @@ export default function BurgerMenu() {
                     </BurgerMenuItem>
                     <BurgerMenuItem
                       text="My Reviews"
-                      href="/restaurants/favorites"
+                      href="/profile"
                       onClick={toggle}
                     >
                       <MdOutlineReviews />
                     </BurgerMenuItem>
                   </BurgerMenuSection>
                 </div>
-                <div className="self-center border-b w-[90%]">
-                  <BurgerMenuSection sectionTitle="Discover">
-                    <BurgerMenuItem
-                      text="Top Rated Restaurants"
-                      href="/restaurants/favorites"
-                      onClick={toggle}
-                    >
-                      <IoTrophyOutline />
-                    </BurgerMenuItem>
-                    <BurgerMenuItem
-                      text="Map View"
-                      href="/profile"
-                      onClick={toggle}
-                    >
-                      <LiaMapMarkedAltSolid />
-                    </BurgerMenuItem>
-                  </BurgerMenuSection>
-                </div>
               </IconContext.Provider>
+              {session && (
+                <button
+                  className="border border-black/60 rounded-lg p-2 w-[90%] mx-auto"
+                  onClick={() => {
+                    signOut();
+                  }}
+                >
+                  Log out
+                </button>
+              )}
             </motion.div>
           </div>
         )}
