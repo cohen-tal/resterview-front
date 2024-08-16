5a0ce200af7324422cb122c68439bbb2c7ace55a
Tal Cohen <cohental63@gmail.com>
2024-08-16 15:54:07 +0300

    add error boundary

---
+++ src/app/error.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: VoidFunction;
+}) {
+  <div>
+    <h2>Something went wrong!</h2>
+    <button onClick={() => reset()}>Try again</button>
+  </div>;
+}
